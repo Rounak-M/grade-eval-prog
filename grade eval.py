79e847f6b34699aa3cbae408b75dec5f6fa58d81
Rounak Miskin <84004493+Rounak-M@users.noreply.github.com>
2025-11-22 12:11:50 +0530

    Update grade eval.py
    2nddd

--- grade eval.py
+++ grade eval.py
@@ -1,8 +1,8 @@
-a = input ("Enter marks for subject 1: ")
-b = input ("Enter marks for subject 2: ")
-c = input ("Enter marks for subject 3: ")
-d = input ("Enter marks for subject 4: ")
-e = input ("Enter marks for subject 5: ")
+a = int (input ("Enter marks for subject 1: "))
+b = int (input ("Enter marks for subject 2: "))
+c = int (input ("Enter marks for subject 3: "))
+d = int (input ("Enter marks for subject 4: "))
+e = int (input ("Enter marks for subject 5: "))
 
 avg = a + b + c + d + e / 5
 

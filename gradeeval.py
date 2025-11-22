1df1b1cfab67c2546cc6402c9638aef57a040e93
Rounak Miskin <84004493+Rounak-M@users.noreply.github.com>
2025-11-22 12:19:13 +0530

    Update gradeeval.py
    changed

--- gradeeval.py
+++ gradeeval.py
@@ -1,18 +1,25 @@
-a = int (input ("Enter marks for subject 1: "))
-b = int (input ("Enter marks for subject 2: "))
-c = int (input ("Enter marks for subject 3: "))
-d = int (input ("Enter marks for subject 4: "))
-e = int (input ("Enter marks for subject 5: "))
+import sys
 
-avg = a + b + c + d + e / 5
+try:
+    if len(sys.argv) != 6:
+        print("Usage: python marks.py <m1> <m2> <m3> <m4> <m5>")
+        sys.exit(1)
 
-print ("Average of 5 subjects: ", avg)
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    c = int(sys.argv[3])
+    d = int(sys.argv[4])
+    e = int(sys.argv[5])
 
-if (avg > 80):
-  print ("Grade: A")
-elif (avg > 60 or avg < 80):
-  print ("Grade: B")
-elif (avg > 40 or avg < 60):
-  print ("Grade: C")
-else:
-  print ("You Failed.")
+    avg = (a + b + c + d + e) / 5
+
+    print("Average of 5 subjects:", avg)
+
+    if avg > 80:
+        print("Grade: A")
+    elif 60 < avg <= 80:
+        print("Grade: B")
+    elif 40 < avg <= 60:
+        print("Grade: C")
+    else:
+        print("You Failed.")
